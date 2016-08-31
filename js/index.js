cac2564cf312e078f3db4948d90abbc3ca25a8ff
Ammar Akhtar <ammar-a@users.noreply.github.com>
2016-08-30 23:24:32 -0400

    UI Improvements

--- js/index.js
+++ js/index.js
@@ -1,19 +1,40 @@
-$('nav a').on('click', function(e) {                 // User clicks nav link
-  e.preventDefault();                                // Stop loading new link
-  var url = this.href;                               // Get value of href
+$( function() {
+    $.ajaxSetup({
+      cache: true // faster scripts
+    });
 
-  $('nav a.current').removeClass('current');         // Clear current indicator
-  $(this).addClass('current');                       // New current indicator
+  $('nav a').on('click', function(e) {                 // User clicks nav link
+    e.preventDefault();                                // Stop loading new link
+    var url = this.href;                               // Get value of href
 
-  $('#content').remove(); 
-  $('#container').load(url + " #content").hide().fadeIn('slow');
+    $('nav a.current').removeClass('current');         // Clear current indicator
+    $(this).addClass('current');                       // New current indicator
 
-  if (url.slice(-13) == "practice.html") {
-    $.getScript("js/practice.js");
-  } else if (url.slice(-16) == "certificate.html") {
-    $.getScript("js/certificate.js");
-    $.getScript("https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js");
-  } else {
-  		$.getScript("js/repeatPictures.js");
+    $('#content').remove(); 
+    $('#container').load(url + " #content").hide().fadeIn(1500); // Load content with AJAX
+
+
+    if (url.slice(-13) == "practice.html") {
+      $.getScript("js/practice.js");
+      $.getScript("js/ui-spinner-behaviour.js");
+
+    } else if (url.slice(-16) == "certificate.html") {
+      $.getScript("js/certificate.js");
+      $.getScript("js/ui-spinner-behaviour.js");
+      $('#angular').remove(); //Remove any previous angular script tag
+      $('<script id="angular" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>').insertAfter('section'); // Reload the script
+    } else {
+        $.getScript("js/repeatPictures.js");
+    }
+  });
+
+  if ($('nav a')[2].scrollWidth - $('nav a').innerWidth() > 5 ) {
+      //Text has over-flowed
+      console.log($('nav a')[2].scrollWidth);
+      console.log($('nav a').innerWidth());
+      $('nav a').css('width', '40%'); // stack the tabs
+      $('nav a').css('font-size', '0.5em');
+
   }
+  
 });
