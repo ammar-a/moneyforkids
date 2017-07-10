acf1fec4806b4afc1ae0ee8ab642b36d39234608
Ammar Akhtar <ammar.akhtar.01@gmail.com>
2017-07-09 21:44:43 -0400

    Angular fix

--- js/boot_angular.js
+++ js/boot_angular.js
@@ -1,7 +1,6 @@
 angular.module('myApp', [])
   .controller('MyController', ['$scope', function ($scope) {
-    $scope.greetMe = 'World';
-  }]);
+}]);
 angular.element(function() {
       angular.bootstrap(document, ['myApp']);
 });
