45333e7b64cc90858c8e5f2c20226ac72c1f121c
Ammar Akhtar <ammar.akhtar.01@gmail.com>
2017-07-09 22:19:44 -0400

    Another test to fix angular

--- js/boot_angular.js
+++ js/boot_angular.js
@@ -1,8 +1,6 @@
-angular.module('myApp', [])
-  .controller('MyController', ['$scope', function ($scope) {
-    $scope.greetMe = '';
-  }]);
-
-angular.element(function() {
-  angular.bootstrap(document, ['myApp']);
+var app = angular.module('demo', [])
+.controller('WelcomeController', function($scope) {
+    $scope.name = '';
 });
+var elem = document.getElementById("root-ng")
+angular.bootstrap(elem, ['demo']);
